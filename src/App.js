0f8d5f43a919f95d6f9ddcaf50d31b2b0499ec2d
C1321766 Thiago Candido Fernandes <C1321766@interno.bb.com.br>
2021-11-04 15:58:06 -0300

    add component to page layour hero

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import './App.scss';
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchHeroes } from './@redux/slice/someHeroesSlice';
 import { addHero, changeVisible } from './@redux/slice/heroSlice';
 import ClipLoader from "react-spinners/ClipLoader";
 import CardHeroes from './components/CardHero/card';
 import Modal from './components/Modal/modal';
 import CardInformation from './components/CardInformation/CardInformation';
+import SearchInput from './components/SearchInput/SearchInput';
 
 function App() {
   const { status, heroes: { results } } = useSelector((state) => state?.sameHeroes);
   const { visible, hero } = useSelector(state => state.hero);
+  const [textSearch, setTextSearch ] = useState('');
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
-    if ( status === 'idle') {
+    if (status === 'idle') {
       dispatch(fetchHeroes());
     }
-  },[dispatch, status]);
+  }, [dispatch, status]);
+
+  const lowecaseSearch = textSearch?.toLowerCase()
+
+  const filterHeros = results?.filter(hero => 
+    hero?.name.toLowerCase().includes(lowecaseSearch)
+    );
 
   const renderCard = () => {
     return (
-      results?.map((hero, index) => (
+      filterHeros?.map(hero => (
         <CardHeroes
-          key={index}
+          key={hero.id}
           name={hero.name}
           imageURl={hero.image.url}
           onClick={() => dispatch(addHero(hero))}
@@ -43,11 +51,20 @@ function App() {
         />
       </Modal>
       {status === 'loading'
-      ? <ClipLoader size={200}/> 
-      :(
-        <div className='render-card'>
-          {renderCard()}
-        </div>)
+        ? <ClipLoader size={200} />
+        : (
+          <div className='container'>
+            <SearchInput 
+              value={textSearch}
+              onChange={search => setTextSearch(search)}
+              label='Pesquise seu herói...'
+              handleSearch={() => console.log('pesquisou')}
+            />
+            <div className='render-card'>
+              {renderCard()}
+            </div>
+          </div>
+        )
       }
     </div>
   )
